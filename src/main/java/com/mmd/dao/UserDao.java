ba718c4c0fa0b08b46e513e66e609e4b5fac2228
Administrator <1908750952@qq.com>
2018-11-30 10:47:11 +0800

    Merge branch 'master' of https://github.com/devinbo/mmd into dev_ldw
    # Conflicts:
    #	.idea/dataSources.xml
    #	.idea/dataSources/e647d264-5952-4131-99cc-f48d6c692c61.xml
    #	src/main/java/com/mmd/dao/UserDao.java
    #	src/main/java/com/mmd/service/impl/UserServiceImpl.java

--- src/main/java/com/mmd/dao/UserDao.java
+++ src/main/java/com/mmd/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.mmd.dao;
 
+import com.mmd.model.Shipaddress;
 import com.mmd.model.User;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserDao {
     List<User> getUser(User user);
@@ -19,4 +21,20 @@ public interface UserDao {
     void updateUser(User user);
 
     int checkPhone(User user);
+
+    User findUserWithNameAndPass(User user);
+
+    List<Shipaddress> queryAddress(@Param("uid") String uid);
+
+    List<Shipaddress> queryAddressByUid(@Param("uid") String uid);
+
+    Shipaddress getShipAddress(@Param("uid") String uid);
+
+    void delAddress(@Param("id") String id);
+
+    int getAddressNumsWithCids(@Param("ids") List<String> ids);
+
+    void updAddress(Shipaddress shipaddress);
+
+    List<Map<String, Object>> getUserByKey(@Param("key") String key);
 }
